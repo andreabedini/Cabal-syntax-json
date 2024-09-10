3eb39b14b01442fa232cdebd66da39f1012ebc4e
Andrea Bedini <andrea@andreabedini.com>
2024-09-10 22:39:56 +0800

    Remove ason dependency and re-organise the code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,232 @@
-module Main where
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -Wall #-}
 
-import Data.Aeson.Text (encodeToLazyText)
-import qualified Data.Text.Lazy.IO as TL
-import Distribution.Aeson (jsonGenericPackageDescription)
+module Main (main) where
+
+import Data.ByteString.Lazy qualified as BL
+import Data.List (foldl1')
+import Distribution.CabalSpecVersion
+import Distribution.Compat.Lens hiding ((.=))
+import Distribution.Compat.Newtype
+import Distribution.FieldGrammar
+import Distribution.Fields
+import Distribution.PackageDescription
+import Distribution.PackageDescription.FieldGrammar
 import Distribution.Simple.PackageDescription (readGenericPackageDescription)
-import qualified Distribution.Verbosity as Verbosity
+import Distribution.Utils.Generic (fromUTF8BS)
+import Distribution.Utils.Json
+import Distribution.Utils.ShortText qualified as ST
+import Distribution.Verbosity (normal)
 import System.Environment (getArgs)
+import ToJSON
 
 main :: IO ()
 main = do
-    fn : _ <- getArgs
-    gpd <- readGenericPackageDescription Verbosity.normal fn
-    TL.putStrLn $ encodeToLazyText $ jsonGenericPackageDescription gpd
+  fn : _ <- getArgs
+  dofile fn
+
+dofile :: FilePath -> IO ()
+dofile fn = do
+  gpd <- readGenericPackageDescription normal fn
+  BL.putStr $ renderJson $ jsonGenericPackageDescription gpd
+
+newtype JSONFieldGrammar s a = JsonFG
+  { fieldGrammarJSON :: CabalSpecVersion -> [Condition ConfVar] -> s -> [Pair]
+  }
+  deriving (Functor)
+
+type JSONFieldGrammar' s = JSONFieldGrammar s s
+
+jsonFieldGrammar :: CabalSpecVersion -> [Condition ConfVar] -> JSONFieldGrammar s a -> s -> [Pair]
+jsonFieldGrammar v cs fg = fieldGrammarJSON fg v cs
+
+instance Applicative (JSONFieldGrammar s) where
+  pure _ = JsonFG (\_ _ _ -> mempty)
+  JsonFG f <*> JsonFG x = JsonFG (\v cs s -> f v cs s <> x v cs s)
+
+instance FieldGrammar ToJSON JSONFieldGrammar where
+  blurFieldGrammar :: ALens' a b -> JSONFieldGrammar b d -> JSONFieldGrammar a d
+  blurFieldGrammar f (JsonFG fg) = JsonFG $ \v cs ->
+    fg v cs . aview f
+
+  uniqueFieldAla :: (ToJSON b, Newtype a b) => FieldName -> (a -> b) -> ALens' s a -> JSONFieldGrammar s a
+  uniqueFieldAla fn _pack l = JsonFG $ \_v cs ->
+    jsonField cs fn . toJSON . pack' _pack . aview l
+
+  booleanFieldDef :: FieldName -> ALens' s Bool -> Bool -> JSONFieldGrammar s Bool
+  booleanFieldDef fn l def = JsonFG $ \_v cs s ->
+    let b = aview l s
+     in if b == def
+          then mempty
+          else jsonField cs fn (toJSON b)
+
+  optionalFieldAla :: (ToJSON b, Newtype a b) => FieldName -> (a -> b) -> ALens' s (Maybe a) -> JSONFieldGrammar s (Maybe a)
+  optionalFieldAla fn _pack l = JsonFG $ \_ cs s ->
+    case aview l s of
+      Nothing -> mempty
+      Just a -> jsonField cs fn (toJSON (pack' _pack a))
+
+  optionalFieldDefAla :: (ToJSON b, Newtype a b, Eq a) => FieldName -> (a -> b) -> ALens' s a -> a -> JSONFieldGrammar s a
+  optionalFieldDefAla fn _pack l def = JsonFG $ \_ cs s ->
+    let x = aview l s
+     in if x == def
+          then mempty
+          else jsonField cs fn (toJSON (pack' _pack x))
+
+  freeTextField :: FieldName -> ALens' s (Maybe String) -> JSONFieldGrammar s (Maybe String)
+  freeTextField fn l = JsonFG $ \_v cs s ->
+    maybe mempty (jsonField cs fn . toJSON) (aview l s)
+
+  freeTextFieldDef :: FieldName -> ALens' s String -> JSONFieldGrammar s String
+  freeTextFieldDef fn l = JsonFG $ \_v cs ->
+    jsonField cs fn . toJSON . aview l
+
+  freeTextFieldDefST :: FieldName -> ALens' s ST.ShortText -> JSONFieldGrammar s ST.ShortText
+  freeTextFieldDefST = defaultFreeTextFieldDefST
+
+  monoidalFieldAla :: (ToJSON b, Monoid a, Newtype a b) => FieldName -> (a -> b) -> ALens' s a -> JSONFieldGrammar s a
+  monoidalFieldAla fn _pack l = JsonFG $ \_v cs ->
+    jsonField cs fn . toJSON . pack' _pack . aview l
+
+  prefixedFields :: FieldName -> ALens' s [(String, String)] -> JSONFieldGrammar s [(String, String)]
+  prefixedFields _fnPfx l = JsonFG $ \_v _cs s ->
+    [n .= JsonString v | (n, v) <- aview l s]
+
+  knownField :: FieldName -> JSONFieldGrammar s ()
+  knownField _ = pure ()
+
+  deprecatedSince :: CabalSpecVersion -> String -> JSONFieldGrammar s a -> JSONFieldGrammar s a
+  deprecatedSince _ _ x = x
+
+  -- TODO: as PrettyFieldGrammar isn't aware of cabal-version: we output the field
+  -- this doesn't affect roundtrip as `removedIn` fields cannot be parsed
+  -- so invalid documents can be only manually constructed.
+  removedIn _ _ x = x
+
+  availableSince _ _ = id
+
+  hiddenField _ = JsonFG (const mempty)
+
+jsonField :: [Condition ConfVar] -> FieldName -> Json -> [Pair]
+jsonField _cs _fn (JsonArray []) = mempty
+jsonField _cs _fn (JsonObject []) = mempty
+jsonField [] fn v = [fromUTF8BS fn .= v]
+jsonField cs fn v = [fromUTF8BS fn .= v']
+  where
+    v' = JsonObject ["if" .= toJSON (foldl1' CAnd cs), "then" .= v]
+
+jsonGenericPackageDescription :: GenericPackageDescription -> Json
+jsonGenericPackageDescription gpd =
+  JsonObject $
+    concat
+      [ jsonPackageDescription v (packageDescription gpd),
+        jsonSetupBInfo v (setupBuildInfo (packageDescription gpd)),
+        jsonGenPackageFlags v (genPackageFlags gpd),
+        jsonCondLibrary v (condLibrary gpd),
+        jsonCondSubLibraries v (condSubLibraries gpd),
+        jsonCondForeignLibs v (condForeignLibs gpd),
+        jsonCondExecutables v (condExecutables gpd),
+        jsonCondTestSuites v (condTestSuites gpd),
+        jsonCondBenchmarks v (condBenchmarks gpd)
+      ]
+  where
+    v = specVersion $ packageDescription gpd
+
+jsonPackageDescription :: CabalSpecVersion -> PackageDescription -> [Pair]
+jsonPackageDescription v pd =
+  jsonFieldGrammar v [] packageDescriptionFieldGrammar pd
+    ++ ["source-repos" .= JsonObject (jsonSourceRepos v (sourceRepos pd))]
+
+jsonSourceRepos :: CabalSpecVersion -> [SourceRepo] -> [Pair]
+jsonSourceRepos v = foldMap (jsonSourceRepo v)
+
+jsonSourceRepo :: CabalSpecVersion -> SourceRepo -> [Pair]
+jsonSourceRepo v repo =
+  jsonFieldGrammar v [] (sourceRepoFieldGrammar (repoKind repo)) repo
+
+jsonSetupBInfo :: CabalSpecVersion -> Maybe SetupBuildInfo -> [Pair]
+jsonSetupBInfo _ Nothing = mempty
+jsonSetupBInfo v (Just sbi) =
+  -- TODO: make this configurable
+  -- \| defaultSetupDepends sbi = mempty
+  ["custom-setup" .= JsonObject vs | not (null vs)]
+  where
+    vs = jsonFieldGrammar v [] (setupBInfoFieldGrammar False) sbi
+
+jsonGenPackageFlags :: CabalSpecVersion -> [PackageFlag] -> [Pair]
+jsonGenPackageFlags v flags = ["flags" .= flags' | not (null flags)]
+  where
+    flags' =
+      JsonObject
+        [ unFlagName name .= JsonObject (jsonFieldGrammar v [] (flagFieldGrammar name) flag)
+          | flag@(MkPackageFlag name _ _ _) <- flags
+        ]
+
+jsonCondLibrary :: CabalSpecVersion -> Maybe (CondTree ConfVar [Dependency] Library) -> [Pair]
+jsonCondLibrary _ Nothing = mempty
+jsonCondLibrary v (Just condTree) = ["library" .= mainlibJson]
+  where
+    mainlibJson = jsonCondTree2 v (libraryFieldGrammar LMainLibName) condTree
+
+jsonCondSubLibraries :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] Library)] -> [Pair]
+jsonCondSubLibraries v libs = ["sub-libraries" .= sublibsJson]
+  where
+    sublibsJson =
+      JsonObject
+        [ unUnqualComponentName n .= jsonCondTree2 v (libraryFieldGrammar $ LSubLibName n) condTree
+          | (n, condTree) <- libs
+        ]
+
+jsonCondForeignLibs :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] ForeignLib)] -> [Pair]
+jsonCondForeignLibs v flibs = ["foreign-libraries" .= flibsJson | not (null flibs)]
+  where
+    flibsJson =
+      JsonObject
+        [ unUnqualComponentName n .= jsonCondTree2 v (foreignLibFieldGrammar n) condTree | (n, condTree) <- flibs
+        ]
+
+jsonCondExecutables :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] Executable)] -> [Pair]
+jsonCondExecutables v exes = ["executables" .= exesJson | not (null exes)]
+  where
+    exesJson =
+      JsonObject
+        [ unUnqualComponentName n .= jsonCondTree2 v (executableFieldGrammar n) condTree
+          | (n, condTree) <- exes
+        ]
+
+jsonCondTestSuites :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] TestSuite)] -> [Pair]
+jsonCondTestSuites v suites = ["test-suites" .= suitesJson | not (null suites)]
+  where
+    suitesJson =
+      JsonObject
+        [ unUnqualComponentName n .= jsonCondTree2 v testSuiteFieldGrammar (fmap unvalidateTestSuite condTree)
+          | (n, condTree) <- suites
+        ]
+
+jsonCondBenchmarks :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] Benchmark)] -> [Pair]
+jsonCondBenchmarks v suites = ["benchmarks" .= suitesJson | not (null suites)]
+  where
+    suitesJson =
+      JsonObject
+        [ unUnqualComponentName n .= jsonCondTree2 v benchmarkFieldGrammar (fmap unvalidateBenchmark condTree)
+          | (n, condTree) <- suites
+        ]
+
+jsonCondTree2 :: CabalSpecVersion -> JSONFieldGrammar' s -> CondTree ConfVar [Dependency] s -> Json
+jsonCondTree2 v grammar = merge . go []
+  where
+    go cs (CondNode it _ ifs) =
+      jsonFieldGrammar v cs grammar it ++ concatMap (jsonIf cs) ifs
+
+    jsonIf cs (CondBranch c thenTree Nothing) =
+      go (c : cs) thenTree
+    jsonIf cs (CondBranch c thenTree (Just elseTree)) =
+      go (c : cs) thenTree ++ go (CNot c : cs) elseTree
+
+    merge :: [Pair] -> Json
+    merge = JsonObject
---
+++ app/ToJSON.hs
@@ -0,0 +1,183 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module ToJSON where
+
+import Data.Functor.Identity
+import Data.Kind
+import Distribution.Compat.Newtype
+import qualified Distribution.Compat.NonEmptySet as NE
+import Distribution.Compiler
+import Distribution.FieldGrammar
+import Distribution.ModuleName
+import Distribution.PackageDescription (unUnqualComponentName)
+import Distribution.PackageDescription.FieldGrammar
+import Distribution.Pretty
+import Distribution.System
+import Distribution.Types.BenchmarkType
+import Distribution.Types.BuildType
+import Distribution.Types.Condition
+import Distribution.Types.ConfVar
+import Distribution.Types.Dependency
+import Distribution.Types.ExeDependency
+import Distribution.Types.ExecutableScope
+import Distribution.Types.Flag
+import Distribution.Types.ForeignLib
+import Distribution.Types.ForeignLibOption
+import Distribution.Types.ForeignLibType
+import Distribution.Types.LegacyExeDependency
+import Distribution.Types.LibraryName
+import Distribution.Types.LibraryVisibility
+import Distribution.Types.Mixin
+import Distribution.Types.ModuleReexport
+import Distribution.Types.PackageName
+import Distribution.Types.PkgconfigDependency
+import Distribution.Types.SourceRepo
+import Distribution.Types.TestType
+import Distribution.Types.Version
+import Distribution.Types.VersionRange
+import Distribution.Utils.Json
+import Distribution.Utils.Path
+import Language.Haskell.Extension
+
+class ToJSON a where
+  toJSON :: a -> Json
+
+  toJSONList :: [a] -> Json
+  toJSONList = listValue toJSON
+
+class ToJSON1 f where
+  liftToJSON :: (a -> Json) -> ([a] -> Json) -> f a -> Json
+
+instance ToJSON1 [] where
+  liftToJSON _ f = f
+
+-- | Helper function to use with 'liftToJSON', see 'listEncoding'.
+listValue :: (a -> Json) -> [a] -> Json
+listValue f = JsonArray . map f
+
+instance ToJSON Char where
+  toJSON c = JsonString [c]
+  toJSONList = JsonString
+
+instance ToJSON Bool where
+  toJSON = JsonBool
+
+instance (ToJSON a) => ToJSON [a] where
+  toJSON = liftToJSON toJSON toJSONList
+
+instance (ToJSON a, ToJSON b) => ToJSON (a, b) where
+  toJSON (a, b) = JsonArray [toJSON a, toJSON b]
+
+deriving via (a :: Type) instance (ToJSON a) => ToJSON (Identity a)
+
+type Pair = (String, Json)
+
+newtype ViaPretty a = ViaPretty a
+
+instance (Pretty a) => ToJSON (ViaPretty a) where
+  toJSON (ViaPretty a) = JsonString $ prettyShow a
+
+newtype ViaUnpack a = ViaUnpack a
+
+instance (ToJSON o, Newtype o n) => ToJSON (ViaUnpack n) where
+  toJSON (ViaUnpack n) = toJSON $ unpack n
+
+deriving via String instance ToJSON Token
+
+deriving via String instance ToJSON Token'
+
+deriving via String instance ToJSON FilePathNT
+
+deriving via String instance ToJSON CompatFilePath
+
+deriving via ViaUnpack CompatLicenseFile instance ToJSON CompatLicenseFile
+
+deriving via ViaPretty VersionRange instance ToJSON VersionRange
+
+deriving via ViaUnpack TestedWith instance ToJSON TestedWith
+
+deriving via ViaPretty CompilerFlavor instance ToJSON CompilerFlavor
+
+deriving via (ViaPretty SpecVersion) instance ToJSON SpecVersion
+
+deriving via (ViaPretty SpecLicense) instance ToJSON SpecLicense
+
+deriving via (ViaUnpack (List sep b a)) instance (ToJSON a) => ToJSON (List sep b a)
+
+deriving via (ViaPretty (SymbolicPath from to)) instance ToJSON (SymbolicPath from to)
+
+deriving via (ViaPretty BuildType) instance ToJSON BuildType
+
+deriving via (ViaPretty PackageName) instance ToJSON PackageName
+
+deriving via (ViaPretty Version) instance ToJSON Version
+
+deriving via (ViaPretty RepoType) instance ToJSON RepoType
+
+deriving via (ViaPretty KnownRepoType) instance ToJSON KnownRepoType
+
+deriving via (ViaPretty Extension) instance ToJSON Extension
+
+deriving via (ViaPretty Language) instance ToJSON Language
+
+deriving via (ViaUnpack (MQuoted a)) instance (ToJSON a) => ToJSON (MQuoted a)
+
+-- deriving via (ViaPretty Dependency) instance ToJSON Dependency
+instance ToJSON Dependency where
+  toJSON (Dependency pn vr libs) =
+    JsonObject ["name" .= toJSON pn, "version" .= toJSON vr, "libs" .= libsJson]
+    where
+      libsJson = JsonArray [libName l | l <- NE.toList libs]
+
+      libName :: LibraryName -> Json
+      libName LMainLibName = toJSON pn
+      libName (LSubLibName n) = toJSON (unUnqualComponentName n)
+
+deriving via (ViaPretty BenchmarkType) instance ToJSON BenchmarkType
+
+deriving via (ViaPretty ForeignLibType) instance ToJSON ForeignLibType
+
+deriving via (ViaPretty TestType) instance ToJSON TestType
+
+deriving via (ViaPretty ExecutableScope) instance ToJSON ExecutableScope
+
+deriving via (ViaPretty ForeignLibOption) instance ToJSON ForeignLibOption
+
+deriving via (ViaPretty LibVersionInfo) instance ToJSON LibVersionInfo
+
+deriving via (ViaPretty ModuleName) instance ToJSON ModuleName
+
+deriving via (ViaPretty ModuleReexport) instance ToJSON ModuleReexport
+
+deriving via (ViaPretty Mixin) instance ToJSON Mixin
+
+deriving via (ViaPretty PkgconfigDependency) instance ToJSON PkgconfigDependency
+
+deriving via (ViaPretty ExeDependency) instance ToJSON ExeDependency
+
+deriving via (ViaPretty LegacyExeDependency) instance ToJSON LegacyExeDependency
+
+deriving via (ViaPretty LibraryVisibility) instance ToJSON LibraryVisibility
+
+deriving via (ViaPretty FlagName) instance ToJSON FlagName
+
+deriving via (ViaPretty Arch) instance ToJSON Arch
+
+deriving via (ViaPretty OS) instance ToJSON OS
+
+instance ToJSON ConfVar where
+  toJSON (OS os) = JsonObject ["os" .= toJSON os]
+  toJSON (Arch arch) = JsonObject ["arch" .= toJSON arch]
+  toJSON (PackageFlag flag) = JsonObject ["flag" .= toJSON flag]
+  toJSON (Impl flavor range) = JsonObject ["impl" .= toJSON flavor, "range" .= toJSON range]
+
+instance (ToJSON c) => ToJSON (Condition c) where
+  toJSON (Var v) = toJSON v
+  toJSON (Lit b) = toJSON b
+  toJSON (CNot c) = JsonObject ["not" .= toJSON c]
+  toJSON (COr l r) = JsonObject ["or" .= JsonArray [toJSON l, toJSON r]]
+  toJSON (CAnd l r) = JsonObject ["and" .= JsonArray [toJSON l, toJSON r]]
