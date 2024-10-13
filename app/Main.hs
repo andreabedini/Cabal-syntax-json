384908f98247c7a93211e980d26b947ff12ceb14
Andrea Bedini <andrea@andreabedini.com>
2024-10-13 15:09:53 +0800

    Less boilerplate

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Main (main) where
@@ -32,8 +34,6 @@ newtype JSONFieldGrammar s a = JsonFG
   { fieldGrammarJSON :: CabalSpecVersion -> s -> [Pair]
   }
   deriving (Functor)
-
-type JSONFieldGrammar' s = JSONFieldGrammar s s
 
 jsonFieldGrammar :: CabalSpecVersion -> JSONFieldGrammar s a -> s -> [Pair]
 jsonFieldGrammar v fg = fieldGrammarJSON fg v
@@ -117,103 +117,64 @@ jsonGenericPackageDescription :: GenericPackageDescription -> Json
 jsonGenericPackageDescription gpd =
   JsonObject $
     concat
-      [ jsonPackageDescription v (packageDescription gpd),
-        jsonSetupBInfo v (setupBuildInfo (packageDescription gpd)),
-        jsonGenPackageFlags v (genPackageFlags gpd),
-        jsonCondLibrary v (condLibrary gpd),
-        jsonCondSubLibraries v (condSubLibraries gpd),
-        jsonCondForeignLibs v (condForeignLibs gpd),
-        jsonCondExecutables v (condExecutables gpd),
-        jsonCondTestSuites v (fmap (fmap (mapTreeData unvalidateTestSuite)) $ condTestSuites gpd),
-        jsonCondBenchmarks v (fmap (fmap (mapTreeData unvalidateBenchmark)) $ condBenchmarks gpd)
+      [ something v (packageDescription gpd),
+        jsonField "source-repos" $ JsonObject $ foldMap (something v) $ sourceRepos (packageDescription gpd),
+        jsonField "custom-build" $ JsonObject $ foldMap (something v) $ setupBuildInfo (packageDescription gpd),
+        jsonField "flags" $ JsonObject $ jsonGenPackageFlags v (genPackageFlags gpd),
+        jsonField "library" $ JsonObject $ foldMap (jsonCondLibrary v) (condLibrary gpd),
+        jsonField "other-libraries" $ JsonObject $ jsonCondLibraries v LSubLibName (condSubLibraries gpd),
+        jsonField "foreign-libraries" $ JsonObject $ jsonCondForeignLibs v (condForeignLibs gpd),
+        jsonField "executables" $ JsonObject $ jsonCondExecutables v (condExecutables gpd),
+        jsonField "test-suites" $ JsonObject $ jsonCondTestSuites v (condTestSuites gpd),
+        jsonField "benchmarks" $ JsonObject $ jsonCondBenchmarks v (condBenchmarks gpd)
       ]
   where
     v = specVersion $ packageDescription gpd
 
-jsonPackageDescription :: CabalSpecVersion -> PackageDescription -> [Pair]
-jsonPackageDescription v pd =
-  jsonFieldGrammar v packageDescriptionFieldGrammar pd
-    <> jsonSourceRepos v (sourceRepos pd)
+class Something a where
+  something :: CabalSpecVersion -> a -> [Pair]
 
-jsonSourceRepos :: CabalSpecVersion -> [SourceRepo] -> [Pair]
-jsonSourceRepos v srs =
-  ["source-repos" .= JsonObject (foldMap (jsonSourceRepo v) srs) | not (null srs)]
+instance Something PackageDescription where
+  something v = jsonFieldGrammar v packageDescriptionFieldGrammar
 
-jsonSourceRepo :: CabalSpecVersion -> SourceRepo -> [Pair]
-jsonSourceRepo v repo =
-  jsonFieldGrammar v (sourceRepoFieldGrammar (repoKind repo)) repo
+instance Something SourceRepo where
+  something v repo = jsonFieldGrammar v (sourceRepoFieldGrammar (repoKind repo)) repo
 
-jsonSetupBInfo :: CabalSpecVersion -> Maybe SetupBuildInfo -> [Pair]
-jsonSetupBInfo _ Nothing = mempty
-jsonSetupBInfo v (Just sbi) =
-  -- TODO: make this configurable
-  -- \| defaultSetupDepends sbi = mempty
-  ["custom-setup" .= JsonObject vs | not (null vs)]
-  where
-    vs = jsonFieldGrammar v (setupBInfoFieldGrammar False) sbi
+instance Something SetupBuildInfo where
+  something v = jsonFieldGrammar v (setupBInfoFieldGrammar False)
 
 jsonGenPackageFlags :: CabalSpecVersion -> [PackageFlag] -> [Pair]
-jsonGenPackageFlags v flags = ["flags" .= flags' | not (null flags)]
-  where
-    flags' =
-      JsonObject
-        [ unFlagName name .= JsonObject (jsonFieldGrammar v (flagFieldGrammar name) flag)
-        | flag@(MkPackageFlag name _ _ _) <- flags
-        ]
+jsonGenPackageFlags v flags =
+  [ unFlagName name .= JsonObject (jsonFieldGrammar v (flagFieldGrammar name) flag)
+  | flag@(MkPackageFlag name _ _ _) <- flags
+  ]
 
-jsonCondLibrary :: CabalSpecVersion -> Maybe (CondTree ConfVar [Dependency] Library) -> [Pair]
-jsonCondLibrary _ Nothing = mempty
-jsonCondLibrary v (Just condTree) = ["library" .= JsonObject mainlibJson]
-  where
-    mainlibJson = jsonCondTree v (libraryFieldGrammar LMainLibName) condTree
+jsonCondLibrary :: CabalSpecVersion -> CondTree ConfVar c Library -> [Pair]
+jsonCondLibrary v = jsonCondTree v (libraryFieldGrammar LMainLibName)
 
--- NOTE: Can we reduce this boilerplate?
+mimap ::
+  (UnqualComponentName -> n) ->
+  (n -> a -> [Pair]) ->
+  (UnqualComponentName, a) ->
+  Pair
+mimap g f (n, a) = (unUnqualComponentName n, JsonObject $ f (g n) a)
 
-jsonCondSubLibraries :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] Library)] -> [Pair]
-jsonCondSubLibraries v libs = ["sub-libraries" .= sublibsJson | not (null libs)]
-  where
-    sublibsJson =
-      JsonObject
-        [ unUnqualComponentName n .= JsonObject (jsonCondTree v (libraryFieldGrammar $ LSubLibName n) condTree)
-        | (n, condTree) <- libs
-        ]
+jsonCondLibraries :: CabalSpecVersion -> (UnqualComponentName -> LibraryName) -> [(UnqualComponentName, CondTree ConfVar c Library)] -> [Pair]
+jsonCondLibraries v f = map . mimap f $ jsonCondTree v . libraryFieldGrammar
 
-jsonCondForeignLibs :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] ForeignLib)] -> [Pair]
-jsonCondForeignLibs v flibs = ["foreign-libraries" .= flibsJson | not (null flibs)]
-  where
-    flibsJson =
-      JsonObject
-        [ unUnqualComponentName n .= JsonObject (jsonCondTree v (foreignLibFieldGrammar n) condTree) | (n, condTree) <- flibs
-        ]
+jsonCondForeignLibs :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar c ForeignLib)] -> [Pair]
+jsonCondForeignLibs v = map . mimap id $ jsonCondTree v . foreignLibFieldGrammar
 
-jsonCondExecutables :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] Executable)] -> [Pair]
-jsonCondExecutables v exes = ["executables" .= exesJson | not (null exes)]
-  where
-    exesJson =
-      JsonObject
-        [ unUnqualComponentName n .= JsonObject (jsonCondTree v (executableFieldGrammar n) condTree)
-        | (n, condTree) <- exes
-        ]
+jsonCondExecutables :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar c Executable)] -> [Pair]
+jsonCondExecutables v = map . mimap id $ jsonCondTree v . executableFieldGrammar
 
-jsonCondTestSuites :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] TestSuiteStanza)] -> [Pair]
-jsonCondTestSuites v suites = ["test-suites" .= suitesJson | not (null suites)]
-  where
-    suitesJson =
-      JsonObject
-        [ unUnqualComponentName n .= JsonObject (jsonCondTree v testSuiteFieldGrammar condTree)
-        | (n, condTree) <- suites
-        ]
+jsonCondTestSuites :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar c TestSuite)] -> [Pair]
+jsonCondTestSuites v = map . mimap id $ \_ -> jsonCondTree v testSuiteFieldGrammar . mapTreeData unvalidateTestSuite
 
-jsonCondBenchmarks :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar [Dependency] BenchmarkStanza)] -> [Pair]
-jsonCondBenchmarks v suites = ["benchmarks" .= suitesJson | not (null suites)]
-  where
-    suitesJson =
-      JsonObject
-        [ unUnqualComponentName n .= JsonObject (jsonCondTree v benchmarkFieldGrammar condTree)
-        | (n, condTree) <- suites
-        ]
+jsonCondBenchmarks :: CabalSpecVersion -> [(UnqualComponentName, CondTree ConfVar c Benchmark)] -> [Pair]
+jsonCondBenchmarks v = map . mimap id $ \_ -> jsonCondTree v benchmarkFieldGrammar . mapTreeData unvalidateBenchmark
 
-jsonCondTree :: CabalSpecVersion -> JSONFieldGrammar' s -> CondTree ConfVar [Dependency] s -> [Pair]
+jsonCondTree :: CabalSpecVersion -> JSONFieldGrammar s s -> CondTree ConfVar c s -> [Pair]
 jsonCondTree v fg = goTree
   where
     goTree (CondNode a _c ifs) =
@@ -227,8 +188,3 @@ jsonCondTree v fg = goTree
         "_then" .= JsonObject (jsonCondTree v fg thenTree),
         "_else" .= JsonObject (jsonCondTree v fg elseTree)
       ]
-
--- jsonIf  (CondBranch c thenTree Nothing) =
---   go  thenTree
--- jsonIf  (CondBranch c thenTree (Just elseTree)) =
---   go  thenTree ++ go (CNot c : cs) elseTree
